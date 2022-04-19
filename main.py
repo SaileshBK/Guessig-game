4324d6da1b26a67f2970e9f6c9170cbec515782f
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-19 10:14:33 +0000

    new ideas

--- main.py
+++ main.py
@@ -5,44 +5,87 @@ print(a)
 
 print("\n")
 x = input("Please choose a number :")
-# if x == '1'or '2' or '3' or '4':
-#     r_ow = np.delete(a, 0, 0)
-#     n = 0
-#     for i in range(1,5):
-#         if i==int(x):
-#             c_ol = np.delete(r_ow, n, 1)
-#             print(c_ol)
-#         n = n + 1
 
+# z=0
+# y=0
+# for i in range(1,5):
+#   if i==int(x):
+#     r_ow = np.delete(a, z, 0)
+#     c_ol = np.delete(r_ow, y, 1)
+#     print(c_ol)
+#   y=y+1
 
+# z=1
+# y=0
+# for i in range(5,9):
+#   if i==int(x):
+#     r_ow = np.delete(a, z, 0)
+#     c_ol = np.delete(r_ow, y, 1)
+#     print(c_ol)
+#   y=y+1
 
-z=0
-y=0
-for i in range(1,5):
-  if i==int(x):
-    r_ow = np.delete(a, z, 0)
-    c_ol = np.delete(r_ow, y, 1)
-    print(c_ol)
-  y=y+1
+# z=2
+# y=0
+# for i in range(9,13):
+#   if i==int(x):
+#     r_ow = np.delete(a, z, 0)
+#     c_ol = np.delete(r_ow, y, 1)
+#     print(c_ol)
+#   y=y+1
 
-z=1
-y=0
-for i in range(5,9):
-  if i==int(x):
-    r_ow = np.delete(a, z, 0)
-    c_ol = np.delete(r_ow, y, 1)
-    print(c_ol)
-  y=y+1
+# z=3
+# y=0
+# for i in range(13,17):
+#   if i==int(x):
+#     r_ow = np.delete(a, z, 0)
+#     c_ol = np.delete(r_ow, y, 1)
+#     print(c_ol)
+#   y=y+1
+
+   
   
-    
+while True:
+  
+  if int(x) in range(1,5):
+    z=0
+    y=0
+    q= range(1,5)
+  elif int(x) in range(5,9):
+    z=1
+    y=0
+    q=range(5,9)
+  elif int(x) in range(9,13):
+    z=2
+    y=0
+    q=range(9,13)
+  else:
+    z=3
+    y=0
+    q=range(13,17)
+  
+  for i in q:
+    if i==int(x):
+      r_ow = np.delete(a, z, 0)
+      c_ol = np.delete(r_ow, y, 1)
+      print(c_ol)
+      print(len(c_ol))
+    y=y+1
+
+  if len(c_ol)==1:
+    break
+  x = input("Please choose a number :")
+  a= c_ol
   
 
 
-      
 
 
 
 
+
+
+
+  
 
       
 # elif int(x)==2:
