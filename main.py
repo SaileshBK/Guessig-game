169353300010595d68809e4bade2b2939aaa0536
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-20 03:05:23 +0000

    done with core concept

--- main.py
+++ main.py
@@ -3,122 +3,92 @@ import numpy as np
 a = np.arange(1, 17).reshape(4, 4)
 print(a)
 
+
 print("\n")
 x = input("Please choose a number :")
-
-# z=0
-# y=0
-# for i in range(1,5):
-#   if i==int(x):
-#     r_ow = np.delete(a, z, 0)
-#     c_ol = np.delete(r_ow, y, 1)
-#     print(c_ol)
-#   y=y+1
-
-# z=1
-# y=0
-# for i in range(5,9):
-#   if i==int(x):
-#     r_ow = np.delete(a, z, 0)
-#     c_ol = np.delete(r_ow, y, 1)
-#     print(c_ol)
-#   y=y+1
-
-# z=2
-# y=0
-# for i in range(9,13):
-#   if i==int(x):
-#     r_ow = np.delete(a, z, 0)
-#     c_ol = np.delete(r_ow, y, 1)
-#     print(c_ol)
-#   y=y+1
-
-# z=3
-# y=0
-# for i in range(13,17):
-#   if i==int(x):
-#     r_ow = np.delete(a, z, 0)
-#     c_ol = np.delete(r_ow, y, 1)
-#     print(c_ol)
-#   y=y+1
-
-   
   
-while True:
+if int(x) in range(1,5):
+  z=0
+  y=0
+  q= range(1,5)
+elif int(x) in range(5,9):
+  z=1
+  y=0
+  q=range(5,9)
+elif int(x) in range(9,13):
+  z=2
+  y=0
+  q=range(9,13)
+else:
+  z=3
+  y=0
+  q=range(13,17)
   
-  if int(x) in range(1,5):
+for i in q:
+  if i==int(x):
+    r_ow = np.delete(a, z, 0)
+    c_ol = np.delete(r_ow, y, 1)
+    print(c_ol)
+      
+  y=y+1
+
+x = input("Please choose a number :")
+  
+mtrx_row = len(c_ol)
+mtrx_col = len(c_ol[0])
+
+for n in range(mtrx_row):
+  for m in range(mtrx_col):
+    if(c_ol[n][m] == int(x)):
+      found_row =n+1
+      found_col =m+1
+
+if ((found_row ==1) and (found_col in range(1,4))):
     z=0
     y=0
-    q= range(1,5)
-  elif int(x) in range(5,9):
+    q= range(1,4)
+
+elif ((found_row ==2) and (found_col in range(1,4))):
     z=1
     y=0
-    q=range(5,9)
-  elif int(x) in range(9,13):
-    z=2
-    y=0
-    q=range(9,13)
-  else:
-    z=3
-    y=0
-    q=range(13,17)
+    q= range(1,4)
+else:
+  z=2
+  y=0
+  q=range(1,4)
+     
+for i in q:
+  if i==found_col:
+    r_ow = np.delete(c_ol, z, 0)
+    c_ol = np.delete(r_ow, y, 1)
+    print(c_ol)
+  y=y+1
+
+
+x = input("Please choose another number :")
   
-  for i in q:
-    if i==int(x):
-      r_ow = np.delete(a, z, 0)
-      c_ol = np.delete(r_ow, y, 1)
-      print(c_ol)
-      print(len(c_ol))
-    y=y+1
+mtrx_row = len(c_ol)
+mtrx_col = len(c_ol[0])
 
-  if len(c_ol)==1:
-    break
-  x = input("Please choose a number :")
-  a= c_ol
-  
+for n in range(mtrx_row):
+  for m in range(mtrx_col):
+    if(c_ol[n][m] == int(x)):
+      found_row =n+1
+      found_col =m+1
 
+if ((found_row ==1) and (found_col in range(1,3))):
+  z=0
+  y=0
+  q= range(1,3)
+else:
+  z=1
+  y=0
+  q= range(1,3)
 
-
-
-
-
-
-
-
-  
-
-      
-# elif int(x)==2:
-#   a_del = np.delete(a, 0, 0)
-#   b_del = np.delete(a_del,1,1)
-#   print(b_del)
-
-# elif int(x)==3:
-#   a_del = np.delete(a, 0, 0)
-#   b_del = np.delete(a_del,2,1)
-#   print(b_del)
-
-# elif int(x)==4:
-#   a_del = np.delete(a, 0, 0)
-#   b_del = np.delete(a_del,3,1)
-#   print(b_del)
-
-# elif int(x)==5:
-#   a_del = np.delete(a, 1, 0)
-#   b_del = np.delete(a_del,0,1)
-#   print(b_del)
-
-# elif int(x)==6:
-#   a_del = np.delete(a, 1, 0)
-#   b_del = np.delete(a_del,1,1)
-#   print(b_del)
-
-# elif int(x)==7:
-#   a_del = np.delete(a, 1, 0)
-#   b_del = np.delete(a_del,2,1)
-#   print(b_del)
-
-# elif int(x)==8:
-#   a_del = np.delete(a, 1, 0)
-#   b_del = np.delete(a_del,3,1)
-#   print(b_del)
+     
+for i in q:
+  if i==found_col:
+    r_ow = np.delete(c_ol, z, 0)
+    c_ol = np.delete(r_ow, y, 1)
+    print(c_ol)
+  y=y+1
