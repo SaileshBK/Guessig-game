0c14465038631d1ae52e8ff711947a28bbded99a
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-20 03:42:43 +0000

    making functions

--- main.py
+++ main.py
@@ -1,102 +1,114 @@
 import numpy as np
 
-a = np.arange(1, 17).reshape(4, 4)
-print(a)
-print("\n")
-x = input("Please choose a number :")
+
+def found_row_col(First_Matrix,x):
+  length_row = len(First_Matrix)
+  length_col = len(First_Matrix[0])
+  
+  for n in range(length_row):
+    for m in range(length_col):
+      if(First_Matrix[n][m] == int(x)):
+        found_row =n+1
+        found_col =m+1
+  
+  if ((found_row ==1) and (found_col in range(1,5))):
+      z=0
+      y=0
+      q= range(1,5)
+  
+  elif ((found_row ==2) and (found_col in range(1,5))):
+      z=1
+      y=0
+      q= range(1,5)
+  
+  elif ((found_row ==3) and (found_col in range(1,5))):
+      z=2
+      y=0
+      q= range(1,5)
+  else:
+    z=3
+    y=0
+    q=range(1,5)
+       
+  for i in q:
+    if i==found_col:
+      r_ow = np.delete(First_Matrix, z, 0)
+      New_Matrix = np.delete(r_ow, y, 1)
+      
+    y=y+1
+
+  return New_Matrix
+  
+
+def main():
+  First_Matrix = np.arange(1, 17).reshape(4, 4)
+  print(First_Matrix)
+  print("\n")
+  x = input("Please choose a number :")
+
+  print(found_row_col(First_Matrix,x))
+
+  x = input("Please choose a number :")
     
-mtrx_row = len(a)
-mtrx_col = len(a[0])
-
-for n in range(mtrx_row):
-  for m in range(mtrx_col):
-    if(a[n][m] == int(x)):
-      found_row =n+1
-      found_col =m+1
-
-if ((found_row ==1) and (found_col in range(1,5))):
-    z=0
-    y=0
-    q= range(1,5)
-
-elif ((found_row ==2) and (found_col in range(1,5))):
-    z=1
-    y=0
-    q= range(1,5)
-
-elif ((found_row ==3) and (found_col in range(1,5))):
+  length_row = len(c_ol)
+  length_col = len(c_ol[0])
+  
+  for n in range(length_row):
+    for m in range(length_col):
+      if(c_ol[n][m] == int(x)):
+        found_row =n+1
+        found_col =m+1
+  
+  if ((found_row ==1) and (found_col in range(1,4))):
+      z=0
+      y=0
+      q= range(1,4)
+  
+  elif ((found_row ==2) and (found_col in range(1,4))):
+      z=1
+      y=0
+      q= range(1,4)
+  else:
     z=2
     y=0
-    q= range(1,5)
-else:
-  z=3
-  y=0
-  q=range(1,5)
-     
-for i in q:
-  if i==found_col:
-    r_ow = np.delete(a, z, 0)
-    c_ol = np.delete(r_ow, y, 1)
-    print(c_ol)
-  y=y+1
-
-x = input("Please choose a number :")
+    q=range(1,4)
+       
+  for i in q:
+    if i==found_col:
+      r_ow = np.delete(c_ol, z, 0)
+      c_ol = np.delete(r_ow, y, 1)
+      print(c_ol)
+    y=y+1
   
-mtrx_row = len(c_ol)
-mtrx_col = len(c_ol[0])
-
-for n in range(mtrx_row):
-  for m in range(mtrx_col):
-    if(c_ol[n][m] == int(x)):
-      found_row =n+1
-      found_col =m+1
-
-if ((found_row ==1) and (found_col in range(1,4))):
+  
+  x = input("Please choose another number :")
+    
+  length_row = len(c_ol)
+  length_col = len(c_ol[0])
+  
+  for n in range(length_row):
+    for m in range(length_col):
+      if(c_ol[n][m] == int(x)):
+        found_row =n+1
+        found_col =m+1
+  
+  if ((found_row ==1) and (found_col in range(1,3))):
     z=0
     y=0
-    q= range(1,4)
-
-elif ((found_row ==2) and (found_col in range(1,4))):
+    q= range(1,3)
+  else:
     z=1
     y=0
-    q= range(1,4)
-else:
-  z=2
-  y=0
-  q=range(1,4)
-     
-for i in q:
-  if i==found_col:
-    r_ow = np.delete(c_ol, z, 0)
-    c_ol = np.delete(r_ow, y, 1)
-    print(c_ol)
-  y=y+1
-
-
-x = input("Please choose another number :")
+    q= range(1,3)
   
-mtrx_row = len(c_ol)
-mtrx_col = len(c_ol[0])
+       
+  for i in q:
+    if i==found_col:
+      r_ow = np.delete(c_ol, z, 0)
+      c_ol = np.delete(r_ow, y, 1)
+      print(c_ol)
+    y=y+1
 
-for n in range(mtrx_row):
-  for m in range(mtrx_col):
-    if(c_ol[n][m] == int(x)):
-      found_row =n+1
-      found_col =m+1
 
-if ((found_row ==1) and (found_col in range(1,3))):
-  z=0
-  y=0
-  q= range(1,3)
-else:
-  z=1
-  y=0
-  q= range(1,3)
-
-     
-for i in q:
-  if i==found_col:
-    r_ow = np.delete(c_ol, z, 0)
-    c_ol = np.delete(r_ow, y, 1)
-    print(c_ol)
-  y=y+1
+if __name__=="__main__":
+  main()
