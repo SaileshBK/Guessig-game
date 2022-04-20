2c931bda8563c022a9badd8b5646616a2ea6e89c
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-20 10:02:02 +0000

    new improved code

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 
-def found_row_col(First_Matrix,x):
+def New_Matrix(First_Matrix,x):
   length_row = len(First_Matrix)
   length_col = len(First_Matrix[0])
+
+  if length_row == 4:
+    Matrix_range = range(1,5)
+  elif length_row == 3:
+    Matrix_range = range(1,4)
+  else:
+    Matrix_range = range(1,3)
+    
   
   for n in range(length_row):
     for m in range(length_col):
@@ -11,26 +19,22 @@ def found_row_col(First_Matrix,x):
         found_row =n+1
         found_col =m+1
   
-  if ((found_row ==1) and (found_col in range(1,5))):
+  if ((found_row ==1) and (found_col in Matrix_range)):
       z=0
       y=0
-      q= range(1,5)
   
-  elif ((found_row ==2) and (found_col in range(1,5))):
+  elif ((found_row ==2) and (found_col in Matrix_range)):
       z=1
       y=0
-      q= range(1,5)
   
-  elif ((found_row ==3) and (found_col in range(1,5))):
+  elif ((found_row ==3) and (found_col in Matrix_range)):
       z=2
       y=0
-      q= range(1,5)
   else:
     z=3
     y=0
-    q=range(1,5)
        
-  for i in q:
+  for i in Matrix_range:
     if i==found_col:
       r_ow = np.delete(First_Matrix, z, 0)
       New_Matrix = np.delete(r_ow, y, 1)
@@ -41,73 +45,86 @@ def found_row_col(First_Matrix,x):
   
 
 def main():
-  First_Matrix = np.arange(1, 17).reshape(4, 4)
-  print(First_Matrix)
+  Main_matrix = np.arange(1, 17).reshape(4, 4)
+  print(Main_matrix)
   print("\n")
   x = input("Please choose a number :")
 
-  print(found_row_col(First_Matrix,x))
+  First_matrix = New_Matrix(Main_matrix,x)
+  print(First_matrix)
 
   x = input("Please choose a number :")
+  Second_matrix = New_Matrix(First_matrix,x)
+  print(Second_matrix)
+
+  x = input("Please choose a number :")
+  Third_matrix = New_Matrix(Second_matrix,x)
+  print(Third_matrix)
+
+
+
+
+
+  
     
-  length_row = len(c_ol)
-  length_col = len(c_ol[0])
+  # length_row = len(c_ol)
+  # length_col = len(c_ol[0])
   
-  for n in range(length_row):
-    for m in range(length_col):
-      if(c_ol[n][m] == int(x)):
-        found_row =n+1
-        found_col =m+1
+  # for n in range(length_row):
+  #   for m in range(length_col):
+  #     if(c_ol[n][m] == int(x)):
+  #       found_row =n+1
+  #       found_col =m+1
   
-  if ((found_row ==1) and (found_col in range(1,4))):
-      z=0
-      y=0
-      q= range(1,4)
+  # if ((found_row ==1) and (found_col in range(1,4))):
+  #     z=0
+  #     y=0
+  #     q= range(1,4)
   
-  elif ((found_row ==2) and (found_col in range(1,4))):
-      z=1
-      y=0
-      q= range(1,4)
-  else:
-    z=2
-    y=0
-    q=range(1,4)
+  # elif ((found_row ==2) and (found_col in range(1,4))):
+  #     z=1
+  #     y=0
+  #     q= range(1,4)
+  # else:
+  #   z=2
+  #   y=0
+  #   q=range(1,4)
        
-  for i in q:
-    if i==found_col:
-      r_ow = np.delete(c_ol, z, 0)
-      c_ol = np.delete(r_ow, y, 1)
-      print(c_ol)
-    y=y+1
+  # for i in q:
+  #   if i==found_col:
+  #     r_ow = np.delete(c_ol, z, 0)
+  #     c_ol = np.delete(r_ow, y, 1)
+  #     print(c_ol)
+  #   y=y+1
   
   
-  x = input("Please choose another number :")
+  # x = input("Please choose another number :")
     
-  length_row = len(c_ol)
-  length_col = len(c_ol[0])
+  # length_row = len(c_ol)
+  # length_col = len(c_ol[0])
   
-  for n in range(length_row):
-    for m in range(length_col):
-      if(c_ol[n][m] == int(x)):
-        found_row =n+1
-        found_col =m+1
+  # for n in range(length_row):
+  #   for m in range(length_col):
+  #     if(c_ol[n][m] == int(x)):
+  #       found_row =n+1
+  #       found_col =m+1
   
-  if ((found_row ==1) and (found_col in range(1,3))):
-    z=0
-    y=0
-    q= range(1,3)
-  else:
-    z=1
-    y=0
-    q= range(1,3)
+  # if ((found_row ==1) and (found_col in range(1,3))):
+  #   z=0
+  #   y=0
+  #   q= range(1,3)
+  # else:
+  #   z=1
+  #   y=0
+  #   q= range(1,3)
   
        
-  for i in q:
-    if i==found_col:
-      r_ow = np.delete(c_ol, z, 0)
-      c_ol = np.delete(r_ow, y, 1)
-      print(c_ol)
-    y=y+1
+  # for i in q:
+  #   if i==found_col:
+  #     r_ow = np.delete(c_ol, z, 0)
+  #     c_ol = np.delete(r_ow, y, 1)
+  #     print(c_ol)
+  #   y=y+1
 
 
 if __name__=="__main__":
